8007843bc5f3440ba3421268dd424fe4bfc7fe71
Piraka-Sushma <sushma.piraka@geminisolutions.com>
2024-07-31 18:40:25 +0530

    merge

--- utils/chunkAndEmbed.py
+++ utils/chunkAndEmbed.py
@@ -1,31 +1,30 @@
 import boto3
-from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.text_splitter import RecursiveCharacterTextSplitter # type: ignore
 from bedrock import create_embeddings
 
 
-def chunk(text , chunkSize=512, overlap=0):
+def chunk(text, chunkSize=512, overlap=32):
     textSplitter = RecursiveCharacterTextSplitter(
       chunk_size=chunkSize,
       chunk_overlap=overlap,
       length_function=len,
       is_separator_regex=False
-  )
+    )
     chunks = textSplitter.split_text(text)
     return chunks
 
-def create_embeddings_chunks(chunks, region_name, model = 'amazon.titan-embed-text-v2:0'):
+def create_chunks(chunks, region_name, model = 'amazon.titan-embed-text-v2:0', chunkSize=512, overlap=32):
     # given a list of chunks return the list of embeddings
     try:
         embeddings = []
         for chunk in chunks:
-            embedding = create_embeddings(chunk, region_name)
+            embedding = create_embeddings(chunk, region_name, model, chunkSize, overlap)
             embeddings.append(embedding)
         return embeddings
     except Exception as e:
         print(f"An error occurred: {e}")
         return None
     
-#chunks = chunk("INdian we are the bbbb kiiis ljdsfesf. njjsnfsdf lokaf adica akdianc kjnakncc cnjae", chunkSize=10, overlap =5)
+# print(chunk("Indian we are the bbbb kiiis ljdsfesf. njjsnfsdf lokaf adica akdianc kjnakncc cnjae", chunkSize=10, overlap =5))
 #embeddings = create_embeddings_chunks(chunks, 'us-east-1')
 # #print(len(embeddings[0]))
-
