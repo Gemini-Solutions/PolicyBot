58baaca9fe1b9cfbd066ad3eb16361cc9bf45d69
gem-adityasharma <aditya.sharma@geminisolutions.com>
2024-08-08 22:25:02 +0530

    updated pdf handler

--- utils/pdf_handler.py
+++ utils/pdf_handler.py
@@ -2,35 +2,37 @@ from utils.text_extraction import extract_text_from_pdf
 from utils.chunk_embed import create_chunks, create_embeddings_for_chunks
 import datetime
 from utils.docDB import insert_many_entry
+import logging
+import uuid
+logging.getLogger().setLevel(logging.INFO)
 
 
 def pdf_handler(bucket_name: str, object_key: str):
-    # Extract Text from PDF
-    extracted_text = extract_text_from_pdf(bucket=bucket_name, document_key=object_key)
-    chunks = create_chunks(extracted_text)
-    chunk_embeddings = create_embeddings_for_chunks(chunks)
-    document = {
-        'bucket_name': bucket_name,
-        'object_key': object_key,
-        'extracted_text': extracted_text,
-        'embedding': chunk_embeddings 
-    }
-    metadata = {
-            'source': 'S3 upload',
-            'upload_date': datetime.datetime.now().isoformat(),
-            'file_name': object_key,
-            
-    }
-    docs = [{
-        'text': chunks[i],
-        'embedding': embedding,
-        'metadata': metadata
-    } for i, embedding in enumerate(chunk_embeddings)]
 
-    insert_many_entry('Table', docs)
+    extracted_text = extract_text_from_pdf(bucket_name, object_key)
 
-    return document
+    if extracted_text is None:
+        raise ValueError("Failed to extract text from the document.")
+    
+    for page, text in extracted_text.items():
+        logging.info(f"Page {page}")
+        chunks = create_chunks(text)
+        embeddings = create_embeddings_for_chunks(chunks)
+        chunk_infos = [{
+                    'name': object_key,
+                    'page_no': page,
+                    'unique_id': str(uuid.uuid1()),  # Generate a unique ID 
+                    'text': chunks[i],
+                    'embedding': embedding
+                } for i, embedding in enumerate(embeddings) if embeddings is not None]
+        
+    try:
+        insert_many_entry('ExtractedTexts', chunk_infos)
+    except:
+        pass
 
+    return chunk_infos
 
 if __name__ == '__main__':
-    pdf_handler('policybot','Access Policy.pdf')
+    chunk_infos = pdf_handler('policybot','Medical Health Insurance Policy.pdf')
+    print(chunk_infos)
--- utils/text_extraction.py
+++ utils/text_extraction.py
@@ -1,72 +1,92 @@
 import os
 import boto3
 import time
+import logging
 from utils.bucket_call import upload_to_s3, save_text_to_s3
 
 
-def extract_text_from_pdf(bucket, document_key):
-    textract_client = boto3.client(service_name='textract')
 
+def start_job(client, s3_bucket_name, object_name):
+    response = None
+    response = client.start_document_text_detection(
+        DocumentLocation={
+            'S3Object': {
+                'Bucket': s3_bucket_name,
+                'Name': object_name
+            }})
+
+    return response["JobId"]
+
+
+def is_job_complete(client, job_id):
+    time.sleep(1)
+    response = client.get_document_text_detection(JobId=job_id)
+    status = response["JobStatus"]
+    logging.info("Job status: {}".format(status))
+
+    while(status == "IN_PROGRESS"):
+        time.sleep(1)
+        response = client.get_document_text_detection(JobId=job_id)
+        status = response["JobStatus"]
+        logging.info("Job status: {}".format(status))
+
+    return status
+
+
+def get_job_results(client, job_id: str):
+    pages = []
+    time.sleep(1)
+    response = client.get_document_text_detection(JobId=job_id)
+    pages.append(response)
+    logging.info("No. of pages received: {}".format(len(pages)))
+    next_token = None
+    if 'NextToken' in response:
+        next_token = response['NextToken']
+
+    while next_token:
+        time.sleep(1)
+        response = client.\
+            get_document_text_detection(JobId=job_id, NextToken=next_token)
+        pages.append(response)
+        logging.info("No. of pages received: {}".format(len(pages)))
+        next_token = None
+        if 'NextToken' in response:
+            next_token = response['NextToken']
+
+    return pages
+
+
+
+def extract_text_from_pdf(bucket: str, document_key: str):
+    
+    textract_client = boto3.client(service_name='textract', region_name='ap-south-1')
     try:
-        response = textract_client.start_document_text_detection(
-            DocumentLocation={'S3Object': {'Bucket': bucket, 'Name': document_key}}
-        )
-        job_id = response.get('JobId')
-        if not job_id:
-            print("Failed to retrieve JobId from response.")
-            return None
-        
-        print(f"Started text detection job with JobId: {job_id}")
-
-        while True:
-            response = textract_client.get_document_text_detection(JobId=job_id)
-            status = response.get('JobStatus')
-            if not status:
-                print("Failed to retrieve JobStatus from response.")
-                return None
-            
-            print(f"Current job status: {status}")
-            if status in ['SUCCEEDED', 'FAILED']:
-                break
-            print("Extracting...")
-            time.sleep(5)
-
+        job_id = start_job(textract_client, bucket, document_key)
+        logging.info("Started job with id: {}".format(job_id))
+        status = is_job_complete(textract_client, job_id)
         if status == 'SUCCEEDED':
+            response = get_job_results(textract_client, job_id)
             extracted_text = {}
-            next_token = None
-            while True:
-                if next_token:
-                    response = textract_client.get_document_text_detection(JobId=job_id, NextToken=next_token)
-                else:
-                    response = textract_client.get_document_text_detection(JobId=job_id)
-                blocks = response.get('Blocks', [])
-                if not blocks:
-                    break
-                
-                for block in blocks:
-                    if block.get('BlockType') == 'LINE':
-                        page_number = block.get('Page')
-                        if page_number is None:
-                            continue
+            for res in response:
+                for block in res['Blocks']:
+                    if block['BlockType'] == 'LINE':
+                        page_number = block['Page']
                         if page_number not in extracted_text:
                             extracted_text[page_number] = ''
-                        extracted_text[page_number] += block.get('Text', '') + '\n'
-                
-                next_token = response.get('NextToken')
-                if not next_token:
-                    break
-
-            return extracted_text
+                        extracted_text[page_number] += f"{block['Text']}\n"
         else:
-            print(f"Text detection failed with status: {status}")
-            print("Detailed response:", response)
+            logging.info(f"Text detection failed with status: {status}")
+            logging.info("Detailed response:", response)
             return None
     except Exception as e:
-        print(f"An error occurred: {e}")
+        logging.info(f"An error occurred: {e}")
         return None
+    return extracted_text
     
-    
-def pdf_upload_and_text_extraction(file_path, bucket):
+def pdf_upload_and_text_extraction(file_path: str, bucket: str):
+    '''
+    For running locally and Testing 
+    '''
     # Upload the PDF to S3
     document_key = upload_to_s3(file_path, bucket)
     if document_key is None:
